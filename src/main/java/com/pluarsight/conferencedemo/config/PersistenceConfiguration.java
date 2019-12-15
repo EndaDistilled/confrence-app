b099538e52bf473feea7402e51ad4c9957494e5a
Enda Brody <enda.brody@distilled.ie>
2019-12-15 16:40:23 +0000

    comment out config

--- src/main/java/com/pluarsight/conferencedemo/config/PersistenceConfiguration.java
+++ src/main/java/com/pluarsight/conferencedemo/config/PersistenceConfiguration.java
@@ -8,11 +8,11 @@ import javax.sql.DataSource;
 
 @Configuration
 public class PersistenceConfiguration {
-    @Bean
-    public DataSource dataSource(){
-        DataSourceBuilder builder = DataSourceBuilder.create();
-        builder.url("jdbc:postgresql://localhost:5432/postgres");
-        System.out.println("My custom dataSource bean has been used");
-        return builder.build();
-    }
+//    @Bean
+//    public DataSource dataSource(){
+//        DataSourceBuilder builder = DataSourceBuilder.create();
+//        builder.url("jdbc:postgresql://localhost:5432/postgres");
+//        System.out.println("My custom dataSource bean has been used");
+//        return builder.build();
+//    }
 }
